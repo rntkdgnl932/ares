e13c8e10357464a70be81b56b2bb487ca65ddaf0
coob <koosanghwi@gmail.com>
2023-07-31 13:08:24 +0900

    ver 1.141

--- data_ares/mymodule/tuto_grow.py
+++ data_ares/mymodule/tuto_grow.py
@@ -828,6 +828,14 @@ def tuto_grow_explain(cla):
                 click_pos_2(945, 145, cla)
 
         if v_.now_chabter == "chap_1_7":
+
+            full_path = "c:\\my_games\\ares\\data_ares\\imgs\\tuto\\explain\\chap_1_7_inventory_title\\inventory_title.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(10, 10, 120, 80, cla, img, 0.7)
+            if imgs_ is not None:
+                click_pos_2(940, 50, cla)
+
             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\tuto\\explain\\chap_1_7_jejak\\jejak_1.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
