93395cee0e90577c86b66742b8b4db39e5b8e8c2
coob <koosanghwi@gmail.com>
2023-08-02 12:51:52 +0900

    ver 1.151

--- data_ares/mymodule/stop_event18.py
+++ data_ares/mymodule/stop_event18.py
@@ -12,7 +12,7 @@ import variable as v_
 def _stop_please(cla):
     import numpy as np
     import cv2
-    from function import imgs_set_, click_pos_reg
+    from function import imgs_set_, click_pos_reg, click_pos_2
 
     try:
         print("_stop_please")
@@ -32,7 +32,12 @@ def _stop_please(cla):
             print("18_1")
             click_pos_reg(imgs_.x, imgs_.y, cla)
 
-
+        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\server_update.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(400, 500, 560, 560, cla, img, 0.7)
+        if imgs_ is not None and imgs_ != False:
+            click_pos_2(480, 590, cla)
 
     except Exception as e:
         print(e)
