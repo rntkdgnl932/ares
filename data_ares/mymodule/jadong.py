1f87bf4d91e001c5bc6ed91d8e9bfc16826373b1
coob <koosanghwi@gmail.com>
2023-08-02 13:24:51 +0900

    ver 1.151

--- data_ares/mymodule/jadong.py
+++ data_ares/mymodule/jadong.py
@@ -370,7 +370,7 @@ def jadong_arrive(cla, where):
                         full_path = "c:\\my_games\\ares\\data_ares\\imgs\\jadong\\attack_ready.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(870, 940, 950, 1030, cla, img, 0.8)
+                        imgs_ = imgs_set_(815, 935, 915, 1030, cla, img, 0.7)
                         if imgs_ is not None and imgs_ != False:
                             walking_count += 1
                             print("loading before walking_count", walking_count)
@@ -417,7 +417,7 @@ def jadong_arrive(cla, where):
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\jadong\\attack_ready.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(870, 940, 950, 1030, cla, img, 0.8)
+                            imgs_ = imgs_set_(815, 935, 915, 1030, cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
                                 walking_count += 1
                                 print("loading after walking_count", walking_count)
--- data_ares/mymodule/test_.py
+++ data_ares/mymodule/test_.py
@@ -32,9 +32,9 @@ def go_test():
     full_path = "c:\\my_games\\ares\\data_ares\\imgs\\jadong\\attack_ready.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(870, 940, 950, 1030, cla, img, 0.8)
+    imgs_ = imgs_set_(815, 935, 915, 1030, cla, img, 0.7)
     if imgs_ is not None and imgs_ != False:
-        print("attack_ready", imgs_)
+        print("사냥터이동중", imgs_)
     else:
         print("attack_ready.")
 
