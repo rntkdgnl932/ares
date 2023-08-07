14d08ffa2e53bff86903d09eb1a113512369679f
coob <koosanghwi@gmail.com>
2023-08-07 10:32:32 +0900

    ver 1.232

--- data_ares/mymodule/main_p.py
+++ data_ares/mymodule/main_p.py
@@ -3124,7 +3124,7 @@ class game_Playing(QThread):
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                             imgs_ = imgs_set_(0, 0, 960, 1030, v_.now_cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
-                                print("매크로를 내려야 실행됨...10초")
+                                print("매크로를 내려야 실행됨...10초", imgs_)
                                 for i in range(10):
                                     full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\ares_start_ready.PNG"
                                     img_array = np.fromfile(full_path, np.uint8)
