52648335bb9a019a64db7fe98b20987033082e70
coob <koosanghwi@gmail.com>
2023-08-12 12:07:45 +0900

    ver 1.26

--- data_ares/mymodule/test_.py
+++ data_ares/mymodule/test_.py
@@ -26,14 +26,14 @@ def go_test():
 
     click_pos_2(840, 90, cla)
     time.sleep(1)
-    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\tuto\\check\\not_movement.PNG"
-    img_array = np.fromfile(full_path, np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(300, 80, 700, 140, cla, img, 0.8)
-    if imgs_ is not None and imgs_ != False:
-        print("not_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movement", imgs_)
-    else:
-        print("not not not not not not_movement")
+    for k in range(10):
+        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\tuto\\check\\not_movement.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(300, 80, 700, 140, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            print("not_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movementnot_movement", imgs_)
+        time.sleep(0.3)
 
 
     full_path = "c:\\my_games\\ares\\data_ares\\imgs\\powerup\\hoilodo_point_2.PNG"
