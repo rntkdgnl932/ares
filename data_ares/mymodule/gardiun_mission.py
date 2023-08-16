d313cd872e12b3e3aeea03d34c1adb357e90c988
coob <koosanghwi@gmail.com>
2023-08-16 23:18:45 +0900

    ver 1.27611

--- data_ares/mymodule/gardiun_mission.py
+++ data_ares/mymodule/gardiun_mission.py
@@ -110,7 +110,7 @@ def gardiun_mission_get(cla, schedule):
                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                                 imgs_ = imgs_set_(400, 400, 700, 700, cla, img, 0.7)
                                 if imgs_ is not None and imgs_ != False:
-                                    print("loding")
+                                    print("loding...")
                                     loading_ares(cla)
                                     break
                                 time.sleep(0.7)
