ef55f0cb520e20b789582682ae0ad731da744b6c
coob <koosanghwi@gmail.com>
2023-08-17 02:07:51 +0900

    ver 1.2765

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -1061,7 +1061,7 @@ def juljun_attack_check_moriagiji(cla, where):
                 full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\gyungjang\\moria_juljun_map_title_1.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(10, 50, 90, 90, cla, img, 0.7)
+                imgs_ = imgs_set_(10, 50, 90, 90, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
                     # 이건 이동 못한 상태
                     print("모리아 기지에서 이동 못한 상태")
