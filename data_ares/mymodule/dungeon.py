7880d4364191a2d1b881012a639b16d2a1b59542
coob <koosanghwi@gmail.com>
2023-08-17 01:25:57 +0900

    ver 1.2764

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -1201,6 +1201,15 @@ def dark_play(cla):
                     if imgs_ is not None and imgs_ != False:
                         print("dark_clear", imgs_)
                         dark_ = True
+                    else:
+                        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\hyubdong\\failed.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(430, 290, 540, 340, cla, img, 0.7)
+                        if imgs_ is not None and imgs_ != False:
+                            print("dark_clear", imgs_)
+                            dark_ = True
+
                 time.sleep(1)
             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\hyubdong\\dark_exit.PNG"
             img_array = np.fromfile(full_path, np.uint8)
