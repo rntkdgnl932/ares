4623923318a3b9ee5c154df075750aadea144530
coob <koosanghwi@gmail.com>
2023-08-16 23:29:54 +0900

    ver 1.27612

--- data_ares/mymodule/gardiun_mission.py
+++ data_ares/mymodule/gardiun_mission.py
@@ -13,11 +13,20 @@ def gardiun_mission_start(cla, schedule):
     try:
         print("gardiun_mission_start")
 
-        result_quest = juljun_quest_check(cla)
-        if result_quest == False:
+        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\gardiun_mission\\gardiun_select.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(735, 955, 815, 1025, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            click_pos_reg(imgs_.x, imgs_.y, cla)
+            time.sleep(1)
             gardiun_mission_get(cla, schedule)
         else:
-            juljun_potion_check(cla)
+            result_quest = juljun_quest_check(cla)
+            if result_quest == False:
+                gardiun_mission_get(cla, schedule)
+            else:
+                juljun_potion_check(cla)
     except Exception as e:
         print(e)
         return 0
