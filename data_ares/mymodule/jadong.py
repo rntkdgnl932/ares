27f99135546636a677288cedc47a6fce877c4eda
coob <koosanghwi@gmail.com>
2023-08-22 01:49:19 +0900

    ver 1.33

--- data_ares/mymodule/jadong.py
+++ data_ares/mymodule/jadong.py
@@ -63,19 +63,6 @@ def jadong_start(cla, where):
                     if imgs_ is not None and imgs_ != False:
                         print("다크디멘젼 모드 : auto_on", imgs_)
 
-                        result_dead = dead_die(cla, where)
-                        if result_dead == True:
-                            dark_mode = False
-
-                        if v_.dark_demen_count > 1:
-                            dark_mode = False
-                        else:
-                            result_dark = dark_play(cla)
-                            print("다크디멘젼 체크 중...", result_dark)
-                            if result_dark == True:
-                                go_spot_in(cla, where)
-
-
                     else:
                         full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\auto_off.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
@@ -85,6 +72,26 @@ def jadong_start(cla, where):
                             print("auto_off", imgs_)
                             click_pos_reg(imgs_.x, imgs_.y, cla)
 
+                    result_dead = dead_die(cla, where)
+                    if result_dead == True:
+                        dark_mode = False
+                    else:
+                        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\juljun.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(400, 580, 560, 630, cla, img, 0.7)
+                        if imgs_ is not None and imgs_ != False:
+                            result_attck = juljun_attack_check(cla, where)
+                            if result_attck == False:
+                                go_spot_in(cla, where)
+
+                    if v_.dark_demen_count > 1:
+                        dark_mode = False
+                    else:
+                        result_dark = dark_play(cla)
+                        print("다크디멘젼 체크 중...", result_dark)
+                        if result_dark == True:
+                            go_spot_in(cla, where)
 
                     time.sleep(60)
 
