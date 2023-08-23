0c41ec452685c3920a424fdf022de06bcec54561
coob <koosanghwi@gmail.com>
2023-08-23 23:55:26 +0900

    ver 1.366

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -1285,7 +1285,7 @@ def dungeon_in_moriagiji(cla, dungeon):
             if imgs_ is not None and imgs_ != False:
 
                 print("모리아기지")
-                y_reg = 865 + (int(where_dungeon[1]) * 35)
+                y_reg = 830 + (int(where_dungeon[1]) * 35)
 
 
 
