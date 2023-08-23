17f81e2b9340e9f55d84ac56694f439deeb7ff91
coob <koosanghwi@gmail.com>
2023-08-24 01:43:04 +0900

    ver 1.37

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -1575,6 +1575,13 @@ def moriagiji_move(cla):
                     imgs_ = imgs_set_(470, 370, 900, 800, cla, img, 0.7)
                     if imgs_ is not None and imgs_ != False:
                         click_pos_reg(imgs_.x, imgs_.y, cla)
+                    else:
+                        full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\gyungjang\\spot_1.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(470, 370, 900, 800, cla, img, 0.7)
+                        if imgs_ is not None and imgs_ != False:
+                            click_pos_reg(imgs_.x, imgs_.y, cla)
                 else:
                     result_out = out_check(cla)
                     if result_out == True:
