e6f1c02a6e9406de48d56c7dca5e83b418356100
coob <koosanghwi@gmail.com>
2023-09-14 04:00:49 +0900

    ver 1.821

--- data_ares/mymodule/action_ares.py
+++ data_ares/mymodule/action_ares.py
@@ -198,6 +198,16 @@ def clean_screen(cla):
                 # 팝업창 끄기
                 _stop_please(cla)
 
+                # 지역퀘스트 골드창 그기
+                full_path = "c:\\my_games\\ares\\data_ares\\imgs\\region_quest\\region_gold_click_ready.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(340, 380, 440, 450, cla, img, 0.7)
+                if imgs_ is not None and imgs_ != False:
+                    click_pos_reg(imgs_.x, imgs_.y - 100, cla)
+                    time.sleep(2)
+                    confirm_all(cla)
+
                 # 메뉴 닫기
                 full_path = "c:\\my_games\\ares\\data_ares\\imgs\\action\\menu\\friend.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
