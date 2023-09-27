24c74066ef867c3e41ede55ea32f174c8b889139
coob <koosanghwi@gmail.com>
2023-09-27 19:14:43 +0900

    ver 1.93

--- data_ares/mymodule/temporary_event.py
+++ data_ares/mymodule/temporary_event.py
@@ -409,9 +409,16 @@ def dungeon_in_event(cla, dungeon):
                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                                 imgs_ = imgs_set_(815, 935, 915, 1030, cla, img, 0.7)
                                 if imgs_ is not None and imgs_ != False:
-                                    print("이동중")
+                                    print("타고 이동중")
                                 else:
-                                    break
+                                    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\jadong\\move_walking.PNG"
+                                    img_array = np.fromfile(full_path, np.uint8)
+                                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                    imgs_ = imgs_set_(815, 935, 915, 1030, cla, img, 0.7)
+                                    if imgs_ is not None and imgs_ != False:
+                                        print("걸어서 이동중")
+                                    else:
+                                        break
                                 time.sleep(1)
 
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\auto_on.PNG"
