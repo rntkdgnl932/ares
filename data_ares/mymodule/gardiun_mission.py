91ae065aa28dc772d8f5e63c2a3b2f43426e7326
coob <koosanghwi@gmail.com>
2023-10-02 08:37:26 +0900

    ver 1.96

--- data_ares/mymodule/gardiun_mission.py
+++ data_ares/mymodule/gardiun_mission.py
@@ -168,7 +168,15 @@ def gardiun_mission_get(cla, schedule):
                                     click_pos_reg(imgs_.x, imgs_.y, cla)
                                     time.sleep(1)
                                 else:
-                                    click_pos_2(800, 1010, cla)
+                                    # 1.96
+                                    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\gyungjang\\moria_map_title.PNG"
+                                    img_array = np.fromfile(full_path, np.uint8)
+                                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                    imgs_ = imgs_set_(10, 10, 120, 100, cla, img, 0.7)
+                                    if imgs_ is not None and imgs_ != False:
+                                        click_pos_2(940, 50, cla)
+                                    else:
+                                        click_pos_2(800, 1010, cla)
                                     time.sleep(1)
 
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\gardiun_mission\\anymore_no_soolyung.PNG"
