b9c7d0369367a3fce4100b2a6f603b4555b15616
coob <koosanghwi@gmail.com>
2024-01-19 17:06:50 +0900

    ver 4.37

--- data_ares/mymodule/gardiun_mission.py
+++ data_ares/mymodule/gardiun_mission.py
@@ -294,12 +294,13 @@ def gardiun_mission_get(cla, schedule):
                         print("jadong_move : break", i)
                         click_pos_reg(imgs_.x, imgs_.y, cla)
                         for c in range(20):
-                            print("gardiun_title : break", c)
+                            print("gardiun_title", c)
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\title\\gardiun_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                             imgs_ = imgs_set_(10, 10, 120, 100, cla, img, 0.8)
                             if imgs_ is not None and imgs_ != False:
+                                print("gardiun_title : break", c)
                                 break
                             time.sleep(2)
                         break
