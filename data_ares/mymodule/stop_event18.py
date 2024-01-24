2ccedfa202477fb50bde1c104ee981916457de9a
coob <koosanghwi@gmail.com>
2024-01-24 15:13:39 +0900

    ver 4.4

--- data_ares/mymodule/stop_event18.py
+++ data_ares/mymodule/stop_event18.py
@@ -70,8 +70,16 @@ def _stop_please(cla):
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                 imgs_ = imgs_set_(420, 460, 530, 510, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
-                    print("season_start3")
+                    print("season_start2")
                     click_pos_reg(imgs_.x, imgs_.y, cla)
+                else:
+                    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\18\\season_start3.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(320, 330, 440, 410, cla, img, 0.7)
+                    if imgs_ is not None and imgs_ != False:
+                        print("season_start3")
+                        click_pos_reg(imgs_.x, imgs_.y, cla)
 
             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\sohwan__confirm.PNG"
             img_array = np.fromfile(full_path, np.uint8)
