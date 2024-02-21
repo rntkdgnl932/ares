96802bf771945840b67062d0234c6316aa9353cd
coob <koosanghwi@gmail.com>
2024-02-21 11:05:35 +0900

    ver 4.8

--- data_ares/mymodule/test_.py
+++ data_ares/mymodule/test_.py
@@ -40,7 +40,12 @@ def go_test():
     elif cla == "four":
         plus = 960 * 3
 
-    hoilodo(cla)
+    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\18\\season_start3.PNG"
+    img_array = np.fromfile(full_path, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    imgs_ = imgs_set_(320, 330, 440, 410, cla, img, 0.7)
+    if imgs_ is not None and imgs_ != False:
+        print("season_start3")
 
 # 300, 8001 700 1000
     # my_property_upload(cla)
