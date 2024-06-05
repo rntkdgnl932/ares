1d16d61fcc570c323801687e491bc7babd433ba1
coob <koosanghwi@gmail.com>
2024-06-05 14:35:55 +0900

    ver 5.562

--- data_ares/mymodule/get_items.py
+++ data_ares/mymodule/get_items.py
@@ -864,14 +864,16 @@ def get_post(cla):
                 time.sleep(0.1)
             else:
                 menu_open(cla)
-                full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\get_post_point_1.PNG"
-                img_array = np.fromfile(full_path, np.uint8)
-                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(800, 350, 830, 375, cla, img, 0.7)
-                if imgs_ is not None and imgs_ != False:
-                    click_pos_reg(imgs_.x - 8, imgs_.y + 10, cla)
-                else:
-                    post_ready = True
+                for i in range(3):
+                    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\get_post_point_1.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(800, 350, 830, 375, cla, img, 0.7)
+                    if imgs_ is not None and imgs_ != False:
+                        click_pos_reg(imgs_.x - 8, imgs_.y + 10, cla)
+                    else:
+                        post_ready = True
+                    time.sleep(0.2)
             time.sleep(1)
 
 
