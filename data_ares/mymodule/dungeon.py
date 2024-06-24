cfa216d78b8997c9ebec63bf2523511618a6afa4
coob <koosanghwi@gmail.com>
2024-06-24 15:21:30 +0900

    ver 5.59

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -2921,7 +2921,7 @@ def juljun_attack_check_talook(cla, where):
             imgs_ = imgs_set_(400, 580, 560, 630, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
                 check_ready = True
-                # 먼저 모리아기지인지 파악
+                # 먼저 타루크 기지인지 파악
                 full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\gyungjang\\talook_juljun_map_title_1_1.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
@@ -2948,6 +2948,14 @@ def juljun_attack_check_talook(cla, where):
                     imgs_ = imgs_set_(40, 50, 90, 90, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         print("타루크 기지인 상태 2")
+                        talook_in = True
+
+                    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\dungeon\\gyungjang\\moria_juljun_map_title_2_2.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(40, 50, 90, 90, cla, img, 0.8)
+                    if imgs_ is not None and imgs_ != False:
+                        print("타루크 기지인 상태 3")
                         talook_in = True
 
 
