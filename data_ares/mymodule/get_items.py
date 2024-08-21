63bf4c3aba16b39585c651ee558b06d5b214a60b
coob <koosanghwi@gmail.com>
2024-08-21 22:24:46 +0900

    ver 5.8401

--- data_ares/mymodule/get_items.py
+++ data_ares/mymodule/get_items.py
@@ -839,7 +839,15 @@ def get_post(cla):
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                             imgs_ = imgs_set_(10, 10, 120, 100, cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
-                                click_pos_2(715, 1015, cla)
+                                full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\post_delete_btn.PNG"
+                                img_array = np.fromfile(full_path, np.uint8)
+                                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                imgs_ = imgs_set_(830, 990, 900, 1040, cla, img, 0.7)
+                                if imgs_ is not None and imgs_ != False:
+                                    click_pos_reg(imgs_.x, imgs_.y, cla)
+
+                                else:
+                                    click_pos_2(715, 1015, cla)
                             else:
                                 click_pos_2(715, 1015, cla)
                                 time.sleep(0.1)
