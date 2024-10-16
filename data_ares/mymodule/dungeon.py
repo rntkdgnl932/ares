887d84f2d19bc25e33c499cab548e9e86549c327
coob <koosanghwi@gmail.com>
2024-10-16 14:35:06 +0900

    ver 5.98

--- data_ares/mymodule/dungeon.py
+++ data_ares/mymodule/dungeon.py
@@ -1680,7 +1680,7 @@ def dungeon_in_moriagiji(cla, dungeon):
                         myQuest_play_add(cla, dungeon)
                         dun_go_ = True
                     else:
-                        click_pos_2(380, 500, cla)
+                        click_pos_2(380 - 40, 500, cla)
                         for i in range(10):
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\title\\moria_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
@@ -2602,7 +2602,7 @@ def dungeon_in_talook(cla, dungeon):
                         myQuest_play_add(cla, dungeon)
                         dun_go_ = True
                     else:
-                        click_pos_2(570, 500, cla)
+                        click_pos_2(570 - 50, 500, cla)
                         for i in range(10):
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\title\\talook_title.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
