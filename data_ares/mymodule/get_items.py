19007f8f987a66314b19feb9e862a256f0b9a5bf
coob <koosanghwi@gmail.com>
2024-11-13 17:38:20 +0900

    ver 6.12

--- data_ares/mymodule/get_items.py
+++ data_ares/mymodule/get_items.py
@@ -538,7 +538,7 @@ def get_event_click(reg_x, reg_y, how, cla):
                 full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\get_event_point_2.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(270, 585, 310, 615, cla, img, 0.7)
+                imgs_ = imgs_set_(270, 620, 310, 650, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
                     # 여기 받기 부분이 how에 따라 x,y 값 달라짐.
 
@@ -579,7 +579,7 @@ def get_event_click(reg_x, reg_y, how, cla):
                             full_path = "c:\\my_games\\ares\\data_ares\\imgs\\get_items\\gyohwan_in_click.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(660, 660, 780, 700, cla, img, 0.77)
+                            imgs_ = imgs_set_(660, 660, 780, 700, cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
                                 click_pos_reg(imgs_.x, imgs_.y, cla)
                                 time.sleep(1)
