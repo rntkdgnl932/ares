05f034caf7a02e443b6453c307150b8c1eb94d9d
coob <koosanghwi@gmail.com>
2024-12-04 22:13:13 +0900

    ver 6.133

--- data_ares/mymodule/test_.py
+++ data_ares/mymodule/test_.py
@@ -42,6 +42,13 @@ def go_test():
     elif cla == "four":
         plus = 960 * 3
 
+    full_path = "c:\\my_games\\ares\\data_ares\\imgs\\18\\18_1.PNG"
+    img_array = np.fromfile(full_path, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    imgs_ = imgs_set_(70, 670, 300, 770, cla, img, 0.7)
+    if imgs_ is not None and imgs_ != False:
+        print("18_1")
+
     full_path = "c:\\my_games\\ares\\data_ares\\imgs\\check\\auto_on.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
