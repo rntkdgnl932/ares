11eb48344045c2052b1aefbca5bdcf334ac95937
coob <koosanghwi@gmail.com>
2024-12-27 18:14:30 +0900

    ver 6.1801

--- data_ares/mymodule/main_p.py
+++ data_ares/mymodule/main_p.py
@@ -3475,8 +3475,8 @@ class game_Playing(QThread):
                                             chaejib_start(v_.now_cla)
 
                                         elif '이벤트' in result_schedule_:
-                                            event_dungeon_start(v_.now_cla, result_schedule_)
-                                            # myQuest_play_add(v_.now_cla, result_schedule_)
+                                            # event_dungeon_start(v_.now_cla, result_schedule_)
+                                            myQuest_play_add(v_.now_cla, result_schedule_)
 
                                         elif '행성파견' in result_schedule_:
                                             dungeon_in_hangsungpagyun(v_.now_cla, result_schedule_)
